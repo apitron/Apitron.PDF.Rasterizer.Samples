56c88ef161f577c32434578218625740f41021f1
apitron <support@apitron.com>
2016-05-16 16:55:57 +0400

    How to set custom ICC color profile for CMYK to RGB color conversion

---
+++ Set_ICCProfile_for_CMYK_to_RGB_conversion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apitron.PDF.Rasterizer;
+using Apitron.PDF.Rasterizer.Configuration;
+using System.Drawing;
+
+namespace Set_ICCProfile_for_CMYK_to_RGB_conversion
+{
+    /// <summary>
+    /// This program demonstrates how to use custom CMYK profile for
+    /// CMYK to RGB transformation.
+    /// IMPORTANT:Don't forget to restore NUGET packages before compilation.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputFileName = "output.png";
+
+            using (Stream stream = File.OpenRead("../../data/cmyk.pdf"), 
+                profileStream = File.OpenRead("../../data/profile.icc"))
+            {
+                // set global CMYK profile to be used for CMYK -> RGB conversion,
+                // it's also possible to pass engine settings as parameter 
+                // during Document object creation.
+                EngineSettings.GlobalSettings.CMYKColorProfile = new IccColorProfile(profileStream);
+
+                using (Document doc = new Document(stream))
+                {
+                    Bitmap result = doc.Pages[0].Render(new Resolution(96, 96), new RenderingSettings());
+                    result.Save(outputFileName);
+                }
+            }
+
+            Process.Start(outputFileName);
+        }
+    }
+}
---
+++ Set_ICCProfile_for_CMYK_to_RGB_conversion/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Set_ICCProfile_for_CMYK_to_RGB_conversion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Apitron LTD.")]
+[assembly: AssemblyProduct("Set_ICCProfile_for_CMYK_to_RGB_conversion")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("Apitron")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee8179e5-614d-43c6-b126-defd596abc80")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
